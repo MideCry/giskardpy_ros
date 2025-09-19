ffe954c8a672641e0fe33fd92a8adda9d441c76e
Simon Stelter <stelter@uni-bremen.de>
2025-09-19 17:30:39 +0200

    bug fix

--- giskardpy_ros/ros2/msg_converter.py
+++ giskardpy_ros/ros2/msg_converter.py
@@ -575,12 +575,16 @@ def world_body_to_geometry(msg: giskard_msgs.WorldBody, color: Color) -> Shape:
 def pose_stamped_to_trans_matrix(
     msg: geometry_msgs.PoseStamped, world: World
 ) -> cas.TransformationMatrix:
-    p = cas.Point3(x=msg.pose.position.x, y=msg.pose.position.y, z=msg.pose.position.z)
+    p = cas.Point3(
+        x_init=msg.pose.position.x,
+        y_init=msg.pose.position.y,
+        z_init=msg.pose.position.z,
+    )
     R = cas.Quaternion(
-        x=msg.pose.orientation.x,
-        y=msg.pose.orientation.y,
-        z=msg.pose.orientation.z,
-        w=msg.pose.orientation.w,
+        x_init=msg.pose.orientation.x,
+        y_init=msg.pose.orientation.y,
+        z_init=msg.pose.orientation.z,
+        w_init=msg.pose.orientation.w,
     ).to_rotation_matrix()
     result = cas.TransformationMatrix.from_point_rotation_matrix(
         point=p,
--- giskardpy_ros/tree/behaviors/init_qp_controller.py
+++ giskardpy_ros/tree/behaviors/init_qp_controller.py
@@ -4,26 +4,46 @@ from typing import List
 import semantic_world.spatial_types.spatial_types as cas
 from giskardpy.data_types.exceptions import EmptyProblemException
 from giskardpy.god_map import god_map
-from giskardpy.qp.constraint import EqualityConstraint, InequalityConstraint, DerivativeInequalityConstraint, \
-    DerivativeEqualityConstraint
+from giskardpy.qp.constraint import (
+    EqualityConstraint,
+    InequalityConstraint,
+    DerivativeInequalityConstraint,
+    DerivativeEqualityConstraint,
+)
 from giskardpy.utils.decorators import record_time
 from py_trees.common import Status
 
 from giskardpy_ros.tree.behaviors.plugin import GiskardBehavior
-from giskardpy_ros.tree.blackboard_utils import catch_and_raise_to_blackboard, GiskardBlackboard
+from giskardpy_ros.tree.blackboard_utils import (
+    catch_and_raise_to_blackboard,
+    GiskardBlackboard,
+)
 
 
 class InitQPController(GiskardBehavior):
     @catch_and_raise_to_blackboard
     @record_time
-
     def update(self):
-        eq_constraints, neq_constraints, eq_derivative_constraints, derivative_constraints, quadratic_weight_gains, linear_weight_gains = god_map.motion_statechart_manager.get_constraints_from_tasks()
+        (
+            eq_constraints,
+            neq_constraints,
+            eq_derivative_constraints,
+            derivative_constraints,
+            quadratic_weight_gains,
+            linear_weight_gains,
+        ) = god_map.motion_statechart_manager.get_constraints_from_tasks()
         try:
-            free_variables = self.get_active_free_symbols(eq_constraints, neq_constraints, eq_derivative_constraints, derivative_constraints)
+            free_variables = self.get_active_free_symbols(
+                eq_constraints,
+                neq_constraints,
+                eq_derivative_constraints,
+                derivative_constraints,
+            )
             GiskardBlackboard().tree.control_loop_branch.add_qp_controller()
         except EmptyProblemException as e:
-            if not god_map.motion_statechart_manager.has_payload_monitors_which_are_not_end_nor_cancel():
+            if (
+                not god_map.motion_statechart_manager.has_payload_monitors_which_are_not_end_nor_cancel()
+            ):
                 raise
             GiskardBlackboard().tree.control_loop_branch.remove_qp_controller()
             return Status.SUCCESS
@@ -40,17 +60,32 @@ class InitQPController(GiskardBehavior):
 
         return Status.SUCCESS
 
-    def get_active_free_symbols(self,
-                                eq_constraints: List[EqualityConstraint],
-                                neq_constraints: List[InequalityConstraint],
-                                eq_derivative_constraints: List[DerivativeEqualityConstraint],
-                                derivative_constraints: List[DerivativeInequalityConstraint]):
+    def get_active_free_symbols(
+        self,
+        eq_constraints: List[EqualityConstraint],
+        neq_constraints: List[InequalityConstraint],
+        eq_derivative_constraints: List[DerivativeEqualityConstraint],
+        derivative_constraints: List[DerivativeInequalityConstraint],
+    ):
         symbols = set()
-        for c in chain(eq_constraints, neq_constraints, eq_derivative_constraints, derivative_constraints):
-            symbols.update(str(s) for s in cas.free_symbols(c.expression))
-        free_variables = list(sorted([v for v in god_map.world.active_degrees_of_freedom if v.symbols.position in symbols],
-                                     key=lambda x: x.symbols.position))
+        for c in chain(
+            eq_constraints,
+            neq_constraints,
+            eq_derivative_constraints,
+            derivative_constraints,
+        ):
+            symbols.update(str(s) for s in c.expression.free_symbols())
+        free_variables = list(
+            sorted(
+                [
+                    v
+                    for v in god_map.world.active_degrees_of_freedom
+                    if v.symbols.position.name in symbols
+                ],
+                key=lambda x: x.symbols.position.name,
+            )
+        )
         if len(free_variables) == 0:
-            raise EmptyProblemException('Goal parsing resulted in no free variables.')
+            raise EmptyProblemException("Goal parsing resulted in no free variables.")
         god_map.degrees_of_freedoms = free_variables
         return free_variables
