25c53b615011f31c6df22a2e9cb26d3388a9c23c
Simon Stelter <stelter@uni-bremen.de>
2025-10-24 12:53:27 +0200

    fixed motion_statechart_inspector.py

--- scripts/tools/motion_statechart_inspector.py
+++ scripts/tools/motion_statechart_inspector.py
@@ -1,7 +1,19 @@
 import sys
 from PyQt5.QtGui import QPainter, QTransform, QKeySequence
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QComboBox, QHBoxLayout, QPushButton, QSizePolicy, \
-    QLabel, QGraphicsView, QGraphicsScene, QGraphicsItem, QShortcut
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QVBoxLayout,
+    QComboBox,
+    QHBoxLayout,
+    QPushButton,
+    QSizePolicy,
+    QLabel,
+    QGraphicsView,
+    QGraphicsScene,
+    QGraphicsItem,
+    QShortcut,
+)
 from PyQt5.QtSvg import QSvgWidget, QGraphicsSvgItem, QSvgRenderer
 from PyQt5.QtCore import Qt, QTimer, QRectF, pyqtSignal
 from pygraphviz import ItemAttribute
@@ -57,7 +69,6 @@ class SvgGrahpicsView(QGraphicsView):
             self.fitInView(self.svg_item, Qt.KeepAspectRatio)
 
 
-
 class DotGraphViewer(QWidget):
     # Add this signal to communicate between threads
     new_message_signal: pyqtSignal = pyqtSignal(object)
@@ -70,7 +81,7 @@ class DotGraphViewer(QWidget):
         self.new_message_signal.connect(self.handle_new_message)
 
         # Initialize the ROS node
-        rospy.init_node('motion_statechart_viewer')
+        rospy.init_node("motion_statechart_viewer")
 
         # Set up the GUI components
         self.svg_widget = SvgGrahpicsView(self)
@@ -81,12 +92,12 @@ class DotGraphViewer(QWidget):
         self.topic_selector.activated.connect(self.on_topic_selector_clicked)
 
         # Navigation buttons
-        self.first_button = QPushButton('First')
-        self.prev_goal_button = QPushButton('Prev Goal')
-        self.prev_button = QPushButton('<')
-        self.next_button = QPushButton('>')
-        self.next_goal_button = QPushButton('Next Goal')
-        self.latest_button = QPushButton('Last')
+        self.first_button = QPushButton("First")
+        self.prev_goal_button = QPushButton("Prev Goal")
+        self.prev_button = QPushButton("<")
+        self.next_button = QPushButton(">")
+        self.next_goal_button = QPushButton("Next Goal")
+        self.latest_button = QPushButton("Last")
 
         # Position label
         self.position_label = QLabel(self)
@@ -109,18 +120,28 @@ class DotGraphViewer(QWidget):
         self.page_up_shortcut = QShortcut(QKeySequence(Qt.Key_PageUp), self)
         self.page_up_shortcut.activated.connect(self.show_next_image)
 
-        self.control_left_shortcut = QShortcut(QKeySequence(Qt.CTRL + Qt.Key_Left), self)
+        self.control_left_shortcut = QShortcut(
+            QKeySequence(Qt.CTRL + Qt.Key_Left), self
+        )
         self.control_left_shortcut.activated.connect(self.show_prev_goal)
-        self.control_right_shortcut = QShortcut(QKeySequence(Qt.CTRL + Qt.Key_Right), self)
+        self.control_right_shortcut = QShortcut(
+            QKeySequence(Qt.CTRL + Qt.Key_Right), self
+        )
         self.control_right_shortcut.activated.connect(self.show_next_goal)
-        self.control_page_down_shortcut = QShortcut(QKeySequence(Qt.CTRL + Qt.Key_PageDown), self)
+        self.control_page_down_shortcut = QShortcut(
+            QKeySequence(Qt.CTRL + Qt.Key_PageDown), self
+        )
         self.control_page_down_shortcut.activated.connect(self.show_prev_goal)
-        self.control_page_up_shortcut = QShortcut(QKeySequence(Qt.CTRL + Qt.Key_PageUp), self)
+        self.control_page_up_shortcut = QShortcut(
+            QKeySequence(Qt.CTRL + Qt.Key_PageUp), self
+        )
         self.control_page_up_shortcut.activated.connect(self.show_next_goal)
 
         self.shift_left_shortcut = QShortcut(QKeySequence(Qt.SHIFT + Qt.Key_Left), self)
         self.shift_left_shortcut.activated.connect(self.show_first_image)
-        self.shift_right_shortcut = QShortcut(QKeySequence(Qt.SHIFT + Qt.Key_Right), self)
+        self.shift_right_shortcut = QShortcut(
+            QKeySequence(Qt.SHIFT + Qt.Key_Right), self
+        )
         self.shift_right_shortcut.activated.connect(self.show_latest_image)
         self.pos1_shortcut = QShortcut(QKeySequence(Qt.Key_Home), self)
         self.pos1_shortcut.activated.connect(self.show_first_image)
@@ -128,12 +149,12 @@ class DotGraphViewer(QWidget):
         self.end_shortcut.activated.connect(self.show_latest_image)
 
         # Add Tooltips for Shortcuts
-        self.next_button.setToolTip('PageUp/RightArrow')
-        self.prev_button.setToolTip('PageDown/LeftArrow')
-        self.next_goal_button.setToolTip('(Ctrl + PageUp)/(Ctrl + RightArrow)')
-        self.prev_goal_button.setToolTip('(Ctrl + PageDown)/(Ctrl + LeftArrow)')
-        self.latest_button.setToolTip('End/(Shift + RightArrow)')
-        self.first_button.setToolTip('Home/(Shift + LeftArrow)')
+        self.next_button.setToolTip("PageUp/RightArrow")
+        self.prev_button.setToolTip("PageDown/LeftArrow")
+        self.next_goal_button.setToolTip("(Ctrl + PageUp)/(Ctrl + RightArrow)")
+        self.prev_goal_button.setToolTip("(Ctrl + PageDown)/(Ctrl + LeftArrow)")
+        self.latest_button.setToolTip("End/(Shift + RightArrow)")
+        self.first_button.setToolTip("Home/(Shift + LeftArrow)")
 
         # Layout for topic selection
         top_layout = QHBoxLayout()
@@ -156,7 +177,7 @@ class DotGraphViewer(QWidget):
         layout.addLayout(nav_layout)
         self.setLayout(layout)
 
-        self.setWindowTitle('Motion Statechart Viewer')
+        self.setWindowTitle("Motion Statechart Viewer")
         self.resize(800, 600)
 
         # Initialize graph history and goal tracking
@@ -174,11 +195,13 @@ class DotGraphViewer(QWidget):
         self.refresh_topics()
 
     def refresh_topics(self) -> None:
-        if self.topic_selector.currentText() == '':
+        if self.topic_selector.currentText() == "":
             # Find all topics of type ExecutionState
             topics = rospy.node.get_topic_names_and_types()
-            target_type = 'giskard_msgs/msg/ExecutionState'
-            execution_state_topics = [name for name, types in topics if target_type in types]
+            target_type = "giskard_msgs/msg/ExecutionState"
+            execution_state_topics = [
+                name for name, types in topics if target_type in types
+            ]
 
             self.topic_selector.clear()
             self.topic_selector.addItems(execution_state_topics)
@@ -194,8 +217,12 @@ class DotGraphViewer(QWidget):
     def on_topic_selected(self, index: int) -> None:
         topic_name = self.topic_selector.currentText()
         if topic_name:
-            rospy.node.create_subscription(msg_type=ExecutionState, topic=topic_name,
-                                           callback=self.on_new_message_received, qos_profile=10)
+            rospy.node.create_subscription(
+                msg_type=ExecutionState,
+                topic=topic_name,
+                callback=self.on_new_message_received,
+                qos_profile=10,
+            )
 
     def on_new_message_received(self, msg: ExecutionState) -> None:
         # Emit signal to handle in main thread
@@ -204,8 +231,11 @@ class DotGraphViewer(QWidget):
     def handle_new_message(self, msg: ExecutionState) -> None:
         # This runs in the main thread
         if len(self.goals) > 0:
-            navigator_at_end = (self.current_goal_index == self.goals[-1]
-                                and self.current_message_index == len(self.graphs_by_goal[self.goals[-1]]) - 1)
+            navigator_at_end = (
+                self.current_goal_index == self.goals[-1]
+                and self.current_message_index
+                == len(self.graphs_by_goal[self.goals[-1]]) - 1
+            )
         else:
             navigator_at_end = True
         # Extract goal_id and group graphs by goal_id
@@ -230,9 +260,15 @@ class DotGraphViewer(QWidget):
         self.update_position_label()
 
         if navigator_at_end:
-            self.display_graph(self.current_goal_index, self.current_message_index, update_position_label=False)
+            self.display_graph(
+                self.current_goal_index,
+                self.current_message_index,
+                update_position_label=False,
+            )
 
-    def display_graph(self, goal_index: int, message_index: int, update_position_label: bool = True) -> None:
+    def display_graph(
+        self, goal_index: int, message_index: int, update_position_label: bool = True
+    ) -> None:
         # Display the graph based on goal and message index
         goal_id = self.goals[goal_index]
         graph = self.graphs_by_goal[goal_id][message_index]
@@ -240,20 +276,20 @@ class DotGraphViewer(QWidget):
         if update_position_label:
             self.update_position_label()
 
-        svg_path = 'graph.svg'
+        svg_path = "graph.svg"
         graph.write_svg(svg_path)
-        graph.write_pdf('graph.pdf')
+        graph.write_pdf("graph.pdf")
         self.svg_widget.load(svg_path)
 
     def update_position_label(self) -> None:
         goal_count = len(self.goals)
         if goal_count == 0:
-            self.position_label.setText('goal 0/0, update 0/0')
+            self.position_label.setText("goal 0/0, update 0/0")
             return
 
         goal_id = self.goals[self.current_goal_index]
         message_count = len(self.graphs_by_goal[goal_id])
-        position_text = f'goal {self.current_goal_index + 1}/{goal_count}, update {self.current_message_index + 1}/{message_count}'
+        position_text = f"goal {self.current_goal_index + 1}/{goal_count}, update {self.current_message_index + 1}/{message_count}"
         # print(position_text)
         self.position_label.setText(position_text)
 
@@ -270,12 +306,18 @@ class DotGraphViewer(QWidget):
             else:
                 if self.current_goal_index > 0:
                     self.current_goal_index -= 1
-                    self.current_message_index = len(self.graphs_by_goal[self.goals[self.current_goal_index]]) - 1
+                    self.current_message_index = (
+                        len(self.graphs_by_goal[self.goals[self.current_goal_index]])
+                        - 1
+                    )
             self.display_graph(self.current_goal_index, self.current_message_index)
 
     def show_next_image(self) -> None:
         if self.goals:
-            if self.current_message_index < len(self.graphs_by_goal[self.goals[self.current_goal_index]]) - 1:
+            if (
+                self.current_message_index
+                < len(self.graphs_by_goal[self.goals[self.current_goal_index]]) - 1
+            ):
                 self.current_message_index += 1
             else:
                 if self.current_goal_index < len(self.goals) - 1:
@@ -298,12 +340,18 @@ class DotGraphViewer(QWidget):
     def show_latest_image(self) -> None:
         if self.goals:
             self.current_goal_index = len(self.goals) - 1
-            self.current_message_index = len(self.graphs_by_goal[self.goals[self.current_goal_index]]) - 1
+            self.current_message_index = (
+                len(self.graphs_by_goal[self.goals[self.current_goal_index]]) - 1
+            )
             self.display_graph(self.current_goal_index, self.current_message_index)
 
 
-if __name__ == '__main__':
+def main():
     app = QApplication(sys.argv)
     viewer = DotGraphViewer()
     viewer.show()
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
